9a2d21451d9867cff618b6a3bd8316bcfe463946
Daniel Eder <daniel.eder@akkodis.com>
2026-08-12 18:16:11 +0200

    feat: implement todo item creation and markdown writing functionality

--- main.c
+++ main.c
@@ -482,6 +482,7 @@ int create_file_if_not_exists(char* fname) {
 }
 
 int add_markdown_item(int argc, char *argv[], const char *filename, const char *prefix, const char *heading) {
+    log_debug("Writing markdown item (c=%d)\n", argc);
     char file_path[DEFAULT_BUFFER_SIZE];
 
     int written = snprintf(file_path, sizeof(file_path), "%s%s%s", g_config.base_dir, get_path_separator(), filename);
@@ -714,9 +715,85 @@ void print_c_standard(void)
 #endif
 }
 
+struct todo {
+    char text[DEFAULT_BUFFER_SIZE * 2];
+    time_t created;
+    unsigned long long id;
+};
+
+int create_todo_from_args(int argc, char *argv[], struct todo *item) {
+    item->text[0] = '\0';
+
+    size_t used = 0;
+
+    for(int i = 0; i < argc; ++i) {
+        int written = snprintf(
+            item->text + used, 
+            sizeof(item->text) - used, 
+            "%s%s", i > 0 ? " " : "",
+            argv[i]);
+        if(written < 0 || (size_t)written >= sizeof(item->text) - used) {
+            log_error("Todo text is too long.\n");
+            return R_ERROR;
+        }
+
+        used += (size_t)written;
+    }
+
+    item->id = 0; // TODO determine next ID
+    item->created = time(NULL);
+
+    return R_OK;
+}
+
+int write_todo(char* filename, struct todo *item) {
+    log_debug("Starting to write todo.\n");
+    char tag_id[DEFAULT_BUFFER_SIZE], tag_created[DEFAULT_BUFFER_SIZE];
+
+    // convert id to a tag
+    log_debug("Converting id to tag.\n");
+    int written = snprintf(tag_id, sizeof(tag_id), "#%s/id/%llu", APP_NAME, item->id);
+    if(written < 0 || (size_t)written >= sizeof(tag_id)) {
+        log_critical("Failed to build todo ID tag.");
+        return R_ERROR;
+    }
+
+    // convert created date to a tag
+    log_debug("Converting cration date to tag.\n");
+    struct tm local_time;
+    if(localtime_s(&local_time, &(item->created)) != 0) {
+        log_error("Failed to get local time.");
+        return R_ERROR;
+    }
+    
+    char date_str[11];
+    if(strftime(date_str, sizeof(date_str), "%Y-%m-%d", &local_time) == 0) {
+        log_error("Feild to format local date.");        
+        return R_ERROR;
+    }
+
+    written = snprintf(tag_created, sizeof(tag_created), "#%s/created/%s", APP_NAME, date_str);
+    if(written < 0 || (size_t)written >= sizeof(tag_created)) {
+        log_critical("Failed to build todo created tag.");
+        return R_ERROR;
+    }
+
+
+    char* argv[] = {item->text, tag_id, tag_created};
+
+    return add_markdown_item(3, argv, filename, "* [ ] ", NULL);
+}
+
 int command_todo_add(int argc, char* argv[]) {
     log_debug("Adding a new todo.\n");
-    if(add_markdown_item(argc, argv, "TODOS.md", "* [ ] ", NULL) != R_OK) {
+    
+    struct todo item;
+
+    if(create_todo_from_args(argc, argv, &item) != R_OK) {
+        return R_ERROR;
+    }
+
+    if(write_todo("TODOS.md", &item) != R_OK) {
         return R_ERROR;
     }
 
