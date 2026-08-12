6b1cb5107ca7ab354ecbe22cbf16d71ca92501b9
Daniel Eder <daniel.eder@akkodis.com>
2026-08-12 15:17:06 +0200

    feat: add commands to display compiler, platform, architecture, and C standard information

--- main.c
+++ main.c
@@ -573,6 +573,74 @@ int command_add(int argc, char* argv[]) {
     return R_OK;
 }
 
+void print_compiler(void)
+{
+#if defined(__clang__)
+    printf("compiler: clang %d.%d.%d\n",
+           __clang_major__,
+           __clang_minor__,
+           __clang_patchlevel__);
+
+#elif defined(__GNUC__)
+    printf("compiler: gcc %d.%d.%d\n",
+           __GNUC__,
+           __GNUC_MINOR__,
+           __GNUC_PATCHLEVEL__);
+
+#elif defined(_MSC_VER)
+    printf("compiler: msvc %d\n", _MSC_VER);
+
+#else
+    printf("compiler: unknown\n");
+#endif
+}
+
+void print_platform(void)
+{
+#if defined(_WIN32)
+    printf("platform: windows");
+#elif defined(__linux__)
+    printf("platform: linux");
+#elif defined(__APPLE__)
+    printf("platform: macos");
+#else
+    printf("platform: unknown");
+#endif
+}
+
+void print_architecture(void) {
+#if defined(__x86_64__) || defined(_M_X64)
+    printf(" x86_64");
+#elif defined(__aarch64__) || defined(_M_ARM64)
+    printf(" arm64");
+#elif defined(__i386__) || defined(_M_IX86)
+    printf(" x86");
+#else
+    printf(" unknown-arch");
+#endif
+
+    printf("\n");
+}
+
+void print_c_standard(void)
+{
+#if defined(__STDC_VERSION__)
+    #if __STDC_VERSION__ >= 202311L
+        printf("c standard: C23\n");
+    #elif __STDC_VERSION__ >= 201710L
+        printf("c standard: C17\n");
+    #elif __STDC_VERSION__ >= 201112L
+        printf("c standard: C11\n");
+    #elif __STDC_VERSION__ >= 199901L
+        printf("c standard: C99\n");
+    #else
+        printf("c standard: pre-C99\n");
+    #endif
+#else
+    printf("c standard: C90\n");
+#endif
+}
+
 int run_command(char* command, int argc, char* argv[]) {
     if(read_config_file() != R_OK) {
         exit(EXIT_CONFIG_ERROR);
@@ -586,6 +654,10 @@ int run_command(char* command, int argc, char* argv[]) {
         return command_add(argc, argv);
     } else if(strcmp(command, "version") == 0) {
          printf("%s %s\n", APP_NAME, APP_VERSION);
+         print_compiler();
+         print_platform();
+         print_architecture();
+         print_c_standard();
     } else if(strcmp(command, "help") == 0) {
         printf("%s is a console scratchpad tool that helps you maintain thoughts, quick notes and todos in a quick fire-and-forget fashion.", APP_NAME);
         printf("usage: %s [options] <command> [options] [subcommand] ...\n", APP_NAME);
