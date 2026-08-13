bafb8b7f238116d5948ca29324d58950a4e0ea17
Daniel Eder <daniel.eder@akkodis.com>
2026-08-13 13:57:07 +0200

    feat: better formatting of topics in today

--- src/cmd_today.c
+++ src/cmd_today.c
@@ -170,12 +170,12 @@ int command_today(int argc, char* argv[]) {
     for(size_t i = 0; i < note_list.count; ++i) {
         char topic[DEFAULT_BUFFER_SIZE];
         if(strlen(note_list.items[i].topic) > 0) {
-            sprintf(topic, ANSI_FG_RGB(180, 140, 255) " [%s]" ANSI_RESET, note_list.items[i].topic);
+            sprintf(topic, ANSI_FG_RGB(180, 140, 255) " 🏷️ %s" ANSI_RESET, note_list.items[i].topic);
         } else {
             sprintf(topic, "");
         }
 
-        printf("    • %-60s%s\n", note_list.items[i].text, topic);
+        printf("    • %s%s\n", note_list.items[i].text, topic);
     }
 
     note_list_free(&note_list);
