f0987cef1ff310076ea1751c30a5df4f67275326
Daniel Eder <daniel.eder@akkodis.com>
2026-08-13 13:41:00 +0200

    feat: colors for log messages

--- src/logging.c
+++ src/logging.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdbool.h>
+#include "cli.h"
 
 extern bool g_debug_enabled;
+
+#define COLOR_SUCCESS ANSI_FG_RGB(100, 210, 140)
+#define COLOR_ERROR   ANSI_FG_RGB(255, 105, 120)
+#define COLOR_WARNING ANSI_FG_RGB(255, 190, 80)
+#define COLOR_INFO    ANSI_FG_RGB(110, 190, 255)
 
 static void log_v(FILE *stream, const char *prefix, const char *fmt, va_list args)
 {
     fputs(prefix, stream);
-    vfprintf(stream, fmt, args);
+    vfprintf(stream, fmt , args);
+    fputs(ANSI_RESET, stream);
 }
 
 void log_error(const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
-    log_v(stderr, "❌ ", fmt, args);
+    log_v(stderr, "❌ " COLOR_ERROR, fmt, args);
     va_end(args);
 }
 
@@ -22,7 +29,7 @@ void log_critical(const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
-    log_v(stderr, "🤯 ", fmt, args);
+    log_v(stderr, "🤯 " COLOR_ERROR, fmt, args);
     va_end(args);
 }
 
@@ -30,7 +37,7 @@ void log_warning(const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
-    log_v(stderr, "⚠️ ", fmt, args);
+    log_v(stderr, "⚠️ " COLOR_WARNING, fmt, args);
     va_end(args);
 }
 
@@ -38,7 +45,7 @@ void log_info(const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
-    log_v(stdout, "ℹ️ ", fmt, args);
+    log_v(stdout, "ℹ️ " COLOR_INFO, fmt, args);
     va_end(args);
 }
 
@@ -46,7 +53,7 @@ void log_success(const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
-    log_v(stdout, "✔️ ", fmt, args);
+    log_v(stdout, "✔️ " COLOR_SUCCESS, fmt, args);
     va_end(args);
 }
 
