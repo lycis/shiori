f74bc366e1fcf62d8cc99da1fc1ace3079aedad7
Daniel Eder <daniel.eder@akkodis.com>
2026-08-17 17:20:00 +0200

    refactor: centralized command definition used in help display

--- src/cmd_help.c
+++ src/cmd_help.c
@@ -1,27 +1,40 @@
 #include <stdio.h>
 #include "common.h"
+#include "commands.h"
 
-int command_help(int argc, char* argv[]) {
-    (void) argc;
-    (void) argv;
-    
-    printf("%s is a console scratchpad tool that helps you maintain thoughts, quick notes and todos in a quick fire-and-forget fashion.", APP_NAME);
-    printf("usage: %s [options] <command> [options] [subcommand] ...\n", APP_NAME);
+int command_help(int argc, char *argv[]) {
+    (void)argc;
+    (void)argv;
+
+    size_t command_count = 0;
+    const struct command_definition *commands = get_commands(&command_count);
+
+    printf(
+        "%s is a console scratchpad tool that helps you maintain "
+        "thoughts, quick notes and todos in a quick fire-and-forget fashion.\n",
+        APP_NAME
+    );
+
+    printf(
+        "usage: %s [options] <command> [options] [subcommand] ...\n",
+        APP_NAME
+    );
+
     printf("\n");
     printf("Options:\n");
-    printf("  %-16s %s\n", "--debug", "Show debug and plumbing output.");
+    printf(
+        "  %-16s %s\n",
+        "--debug",
+        "Show debug and plumbing output."
+    );
+
     printf("\n");
     printf("Available commands:\n");
-    printf("  %-16s %s\n", "init",   "Initialize a new configuration");
-    printf("  %-16s %s\n", "config", "Show or modify configuration");
-    printf("  %-16s %s\n", "add",    "Add a new note or thought to the day");
-    printf("  %-16s %s\n", "capture","Interactively capture notes and todos");
-    printf("  %-16s %s\n", "topic",  "Browse notes by topic");
-    printf("  %-16s %s\n", "tag",    "Find notes and todos by tag");
-    printf("  %-16s %s\n", "todo",   "Manage your todos and tasks");
-    printf("  %-16s %s\n", "today",  "Your overview for the current day");
-    printf("  %-16s %s\n", "console","Start the interactive console");
-    printf("  %-16s %s\n", "help",   "Show this help");
-    printf("  %-16s %s\n", "version","Display current version information");
+
+    for(size_t i = 0; i < command_count; ++i) {
+        printf("  %-16s %s\n", commands[i].name, commands[i].description
+        );
+    }
+
     return R_OK;
 }
--- src/commands.c
+++ src/commands.c
@@ -6,6 +6,127 @@
 #include <string.h>
 #include <stdlib.h>
 
+static const struct command_definition commands[] = {
+    {
+        "init",
+        "Initialize a new configuration",
+        command_init,
+        NULL,
+        0,
+        false
+    },
+    {
+        "config",
+        "Show or modify configuration",
+        command_config,
+        NULL,
+        0,
+        true
+    },
+    {
+        "add",
+        "Add a new note or thought to the day",
+        command_add,
+        NULL,
+        0,
+        true
+    },
+    {
+        "capture",
+        "Interactively capture notes and todos",
+        command_capture,
+        NULL,
+        0,
+        true
+    },
+    {
+        "topic",
+        "Browse notes by topic",
+        command_topic,
+        NULL,
+        0,
+        true
+    },
+    {
+        "tag",
+        "Find notes and todos by tag",
+        command_tag,
+        NULL,
+        0,
+        true
+    },
+    {
+        "todo",
+        "Manage your todos and tasks",
+        command_todo,
+        NULL,
+        0,
+        true
+    },
+    {
+        "today",
+        "Your overview for the current day",
+        command_today,
+        NULL,
+        0,
+        true
+    },
+    {
+        "console",
+        "Start the interactive console",
+        command_console,
+        NULL,
+        0,
+        true
+    },
+    {
+        "util",
+        "Utility and integration commands",
+        command_util,
+        NULL,
+        0,
+        true
+    },
+    {
+        "help",
+        "Show this help",
+        command_help,
+        NULL,
+        0,
+        false
+    },
+    {
+        "version",
+        "Display current version information",
+        command_version,
+        NULL,
+        0,
+        false
+    }
+};
+
+const struct command_definition* get_commands(size_t *count) {
+    if(count != NULL) {
+        *count = sizeof(commands) / sizeof(commands[0]);
+    }
+
+    return commands;
+}
+
+const struct command_definition* find_command(const struct command_definition *commands, size_t command_count, const char *name) {
+    if(commands == NULL || name == NULL) {
+        return NULL;
+    }
+
+    for(size_t i = 0; i < command_count; ++i) {
+        if(strcmp(commands[i].name, name) == 0) {
+            return &commands[i];
+        }
+    }
+
+    return NULL;
+}
+
 int run_command(char* command, int argc, char* argv[]) {
     if(strcmp(command, "version") == 0) {
         return command_version(argc, argv);
